f023aeafdc3a587031ea87fed02d34735d1f4ec3
sabahfallaha <sabahfallaha88@gmail.com>
2023-05-05 10:55:34 +0300

    section card

---
+++ lib/widgets/section_list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../constant.dart';
+import '../model/section_model.dart';
+
+class SectionCard extends StatelessWidget {
+  SectionCard({required this.section, Key? key}) : super(key: key);
+  SectionModel section;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        //  Navigator.pushNamed(context, CourseDetails.id, arguments: course);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: ListTile(
+          // course.name
+          title: Text(
+            section.name,
+            style: TextStyle(
+                color: KSecondaryColor,
+                fontSize: 22,
+                fontWeight: FontWeight.bold),
+          ),
+          // course description
+          subtitle: Text(
+            section.description,
+            style: TextStyle(color: KSecondaryColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
